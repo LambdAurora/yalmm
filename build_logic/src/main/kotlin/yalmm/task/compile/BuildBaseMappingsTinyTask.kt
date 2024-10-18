9d08c43faab0b087275e04057429aeb1030386cf
LambdAurora <email@lambdaurora.dev>
2024-10-18 18:16:42 +0200

    Fix unmatched inner classes from mapped classes.

--- build_logic/src/main/kotlin/yalmm/task/compile/BuildBaseMappingsTinyTask.kt
+++ build_logic/src/main/kotlin/yalmm/task/compile/BuildBaseMappingsTinyTask.kt
@@ -12,6 +12,7 @@ import cuchaz.enigma.translation.mapping.serde.MappingFileNameFormat
 import cuchaz.enigma.translation.mapping.serde.MappingSaveParameters
 import cuchaz.enigma.translation.mapping.tree.EntryTree
 import cuchaz.enigma.translation.mapping.tree.HashEntryTree
+import cuchaz.enigma.translation.representation.entry.ClassEntry
 import cuchaz.enigma.translation.representation.entry.MethodEntry
 import cuchaz.enigma.utils.Utils
 import org.gradle.api.file.RegularFileProperty
@@ -19,9 +20,11 @@ import org.gradle.api.tasks.InputDirectory
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
 import yalmm.Constants
+import yalmm.mapping.InnerClassSolver
 import yalmm.task.DefaultYalmmTask
 import yalmm.task.setup.mapping.MapGameJarTask
 import java.io.File
+import java.util.stream.Collectors
 
 open class BuildBaseMappingsTinyTask : DefaultYalmmTask(Constants.Groups.BUILD) {
 	companion object {
@@ -64,6 +67,46 @@ open class BuildBaseMappingsTinyTask : DefaultYalmmTask(Constants.Groups.BUILD) 
 			}
 		}
 
+		// Add inner classes that are not present in the result mapping tree.
+		val innerClassesMap = jarIndex.entryIndex.classes.parallelStream()
+			.filter { it.isInnerClass }
+			.sorted(Comparator.comparing { it.fullName })
+			.collect(Collectors.toMap(ClassEntry::getOutermostClass, { listOf(it) }, { a, b -> a + b }))
+		for ((outMostParent, innerClasses) in innerClassesMap) {
+			val parentSolver = InnerClassSolver(null, outMostParent);
+			val knownOutMostName = result.get(outMostParent)?.targetName
+
+			if (knownOutMostName != null) {
+				parentSolver.setKnownName(knownOutMostName)
+			}
+
+			for (innerClass in innerClasses) {
+				val hierarchy = listHierarchy(innerClass)
+				var currentParent = parentSolver
+
+				for (part in hierarchy) {
+					var partSolver = currentParent.innerClasses[currentParent.node()]
+
+					if (partSolver == null) {
+						partSolver = InnerClassSolver(currentParent, part)
+
+						val currentKnownOutMostName = result.get(part)?.targetName
+						if (currentKnownOutMostName != null) {
+							partSolver.setKnownName(currentKnownOutMostName)
+						}
+
+						currentParent.innerClasses[part] = partSolver
+					}
+
+					currentParent = parentSolver
+				}
+			}
+
+			if (parentSolver.shouldResolve()) {
+				parentSolver.apply(result)
+			}
+		}
+
 		// Add correct mappings for specialized methods
 		for ((bridge, specialized) in bridgeMethodIndex.bridgeToSpecialized) {
 			val translated = translator.extendedTranslate(bridge)!!
@@ -77,5 +120,15 @@ open class BuildBaseMappingsTinyTask : DefaultYalmmTask(Constants.Groups.BUILD) 
 		val output = this.outputMappings.toPath()
 		Utils.delete(output)
 		MappingCommandsUtil.write(result, "tinyv2:mojang_named:named", output, saveParameters)
+	}
+
+	private fun listHierarchy(node: ClassEntry): List<ClassEntry> {
+		val list = ArrayList<ClassEntry>()
+		var currentNode: ClassEntry? = node
+		while (currentNode?.outerClass != null) {
+			list.add(currentNode)
+			currentNode = currentNode.outerClass
+		}
+		return list.reversed()
 	}
 }
