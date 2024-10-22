5a4b08a28810dd09f098eec40a37520cd7d371bf
LambdAurora <email@lambdaurora.dev>
2024-10-22 20:06:00 +0200

    1.21.2

--- build_logic/src/main/kotlin/yalmm/Constants.kt
+++ build_logic/src/main/kotlin/yalmm/Constants.kt
@@ -2,7 +2,7 @@ package yalmm
 
 object Constants {
 	const val GROUP = "dev.lambdaurora"
-	const val MINECRAFT_VERSION = "1.21.2-rc1"
+	const val MINECRAFT_VERSION = "1.21.2"
 	const val JAVA_VERSION = 21
 
 	object Groups {
