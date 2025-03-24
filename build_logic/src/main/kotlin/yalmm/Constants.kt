b794928a442bf29bd3dd72d5fc40404c6d98ede2
LambdAurora <email@lambdaurora.dev>
2025-03-24 16:11:41 +0100

    Update to Minecraft 1.21.5-rc2.

--- build_logic/src/main/kotlin/yalmm/Constants.kt
+++ build_logic/src/main/kotlin/yalmm/Constants.kt
@@ -2,7 +2,7 @@ package yalmm
 
 object Constants {
 	const val GROUP = "dev.lambdaurora"
-	const val MINECRAFT_VERSION = "1.21.5-rc1"
+	const val MINECRAFT_VERSION = "1.21.5-rc2"
 	const val JAVA_VERSION = 21
 
 	object Groups {
