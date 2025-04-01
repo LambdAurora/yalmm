e83e36470dc1645d27f6b62d11ef476f228ddb77
LambdAurora <email@lambdaurora.dev>
2025-04-01 22:34:09 +0200

    CraftMine???

--- build_logic/src/main/kotlin/yalmm/Constants.kt
+++ build_logic/src/main/kotlin/yalmm/Constants.kt
@@ -2,7 +2,7 @@ package yalmm
 
 object Constants {
 	const val GROUP = "dev.lambdaurora"
-	const val MINECRAFT_VERSION = "1.21.5"
+	const val MINECRAFT_VERSION = "25w14craftmine"
 	const val JAVA_VERSION = 21
 
 	object Groups {
