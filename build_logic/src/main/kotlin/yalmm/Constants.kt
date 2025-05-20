0e04024b967f7106872d0ab5d5badef2cd252cd3
LambdAurora <email@lambdaurora.dev>
2025-05-20 23:34:33 +0200

    Port to 21w20a.

--- build_logic/src/main/kotlin/yalmm/Constants.kt
+++ build_logic/src/main/kotlin/yalmm/Constants.kt
@@ -2,7 +2,7 @@ package yalmm
 
 object Constants {
 	const val GROUP = "dev.lambdaurora"
-	const val MINECRAFT_VERSION = "25w19a"
+	const val MINECRAFT_VERSION = "25w20a"
 	const val JAVA_VERSION = 21
 
 	object Groups {
