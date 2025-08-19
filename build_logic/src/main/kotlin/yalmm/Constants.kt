9f05e8cf319808e1ee236c8af673ac0d936e80bc
LambdAurora <email@lambdaurora.dev>
2025-08-19 18:11:06 +0200

    Update to 25w34b.

--- build_logic/src/main/kotlin/yalmm/Constants.kt
+++ build_logic/src/main/kotlin/yalmm/Constants.kt
@@ -2,7 +2,7 @@ package yalmm
 
 object Constants {
 	const val GROUP = "dev.lambdaurora"
-	const val MINECRAFT_VERSION = "25w33a"
+	const val MINECRAFT_VERSION = "25w34b"
 	const val JAVA_VERSION = 21
 
 	object Groups {
